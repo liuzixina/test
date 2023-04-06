ad601f1c7c27f22c84dae9c67f79b553fd39f32e
Zixin Liu <2137744656@qq.com>
2023-04-07 01:03:02 +0800

    step4

--- practical2/practical2.2/practical2.2/practical2.2.cpp
+++ practical2/practical2.2/practical2.2/practical2.2.cpp
@@ -27,6 +27,7 @@ int main() {
 }
 /* function to read number from standard input (keyboard) */
 void getNumber(int *num) {
+
 	scanf("%d", &num);
 }
 /* function to calculate the sum of two numbers */
