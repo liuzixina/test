590a918c8422f8a1492cf47b0289bf15f8a28071
Zixin Liu <2137744656@qq.com>
2023-04-06 22:58:28 +0800

    first commit for the activity one

--- practical2/practical2/practical2.cpp
+++ practical2/practical2/practical2.cpp
@@ -1,5 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <stdio.h>
+using namespace std;
 
 
 /* The following are function declarations. They _declare_ that the functions exist but they do not _define_ what the functions actually do (the definitions appear after the main function). By declaring these functions here, they may be called in the main before they are defined.
@@ -11,15 +13,18 @@ void function2(int *);
 int main() {
 	int number = 10;
 	/* C1. display the contents of number to the screen */
-
+	std::cout << number;
 	/* C2. display the address of variable number to the screen */
-
+	std::cout << &number;
 	/* C3. call function1 */
-
+	function1(number);
 	/* C4. display the contents of number to the screen */
-
+	std::cout << number;
 	/* C5. call function2 */
+	int*p1 = &number;
+	function2(p1);
 	/* C6. display the contents of number to the screen */
+	std::cout << number;
 	return 0;
 
 }
@@ -36,6 +41,4 @@ void function2(int *numberPtr)
 	/* C11. display the value that the pointer numberPtr points to */
 	/* C12. increment the value stored in number */
 
-	/* C13. display the contents of number to the screen */u
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-sssssssssssssssssssssss
+	/* C13. display the contents of number to the screen */
